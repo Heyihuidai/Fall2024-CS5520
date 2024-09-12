a297010ebb258951c903e2775bac5c8c140bd406
Shuojun Chen <shuojun@Shuojuns-MacBook-Air.local>
2024-09-12 11:40:35 -0700

    add name and change the welcome text

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const appName = "MyAwesomeApp";
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to MyAwesomeApp!</Text>
       <StatusBar style="auto" />
     </View>
   );
