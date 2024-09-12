972411d566c9e07709a13e4e358b6bb4122964d0
Shuojun Chen <shuojun@Shuojuns-MacBook-Air.local>
2024-09-12 11:59:32 -0700

    app_name is passed as a prop in App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
 
@@ -7,8 +8,8 @@ export default function App() {
   
   return (
     <View style={styles.container}>
-      <Text>Welcome to MyAwesomeApp!</Text>
       <StatusBar style="auto" />
+      <Header />
     </View>
   );
 }
