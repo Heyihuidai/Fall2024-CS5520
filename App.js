aef8373786861be7c77b9ffd49f4cd08ec4b5e61
Shuojun Chen <shuojun@Shuojuns-MacBook-Air.local>
2024-09-12 21:00:12 -0700

    adjust and put autoFocus in the file

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
-import Input from './components/Input'; // Adjust the path if necessary
+import Input from './components/Input';
 import { useState } from 'react';
 
 export default function App() {
   const appName = "MyAwesomeApp";
-  const [text, setText] = useState("");
+  const [autoFocus, setAutoFocus] = useState(true);
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name={appName}/>
-      <Input value={text} onChangeText={setText} />
-      <Text>{text}</Text>
+      <Input autoFocus={autoFocus} />
     </View>
   );
 }
