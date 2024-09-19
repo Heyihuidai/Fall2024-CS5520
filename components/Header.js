d4fd386d1e2d4adee92cff2e2db847c98a0127da
Shuojun Chen <shuojun@Shuojuns-MacBook-Air.local>
2024-09-19 12:56:39 -0700

    keep updated

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Header({app_name}) {
+export default function Header({name, children}) {
   return (
     <View>
-      <Text>Welcome to {app_name}!</Text>
+      <Text>Welcome to {name}!</Text>
+      {children}
     </View>
   )
 }
