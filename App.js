4dd717e5fea7776621e9ab0507b223e14bcde7d0
Shuojun Chen <shuojun@Shuojuns-MacBook-Air.local>
2024-09-19 12:06:19 -0700

    debug Button import

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Header from './components/Header';
 import Input from './components/Input';
 import { useState } from 'react';
