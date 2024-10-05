43207ae8c07ffea77bd89da8462c0eb7af482fae
Shuojun Chen <shuojun@Shuojuns-MacBook-Air.local>
2024-10-04 23:14:07 -0700

    add functionality to warning button which should interact with the GoalDetails component, so revise GoalDetails and Home

--- components/GoalDetails.js
+++ components/GoalDetails.js
@@ -1,30 +1,43 @@
-import React from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import { Text, View, SafeAreaView, TouchableOpacity, StyleSheet } from 'react-native';
 import sharedStyles from '../styles/sharedStyles.js';
 
 export default function GoalDetails({ route, navigation }) {
   const { goalObj } = route.params;
+  const [isWarning, setIsWarning] = useState(false);
 
-  const handleMoreDetails = () => {
-    navigation.push('Details', { 
-      goalObj: {
-        ...goalObj,
-        additionalInfo: 'This is some additional information about the goal.'
-      }
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isWarning ? 'Warning!' : goalObj.text,
+      headerLeft: () => (
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text style={{ color: '#fff', marginLeft: 10 }}>All My Goals</Text>
+        </TouchableOpacity>
+      ),
+      headerRight: () => (
+        <TouchableOpacity onPress={() => setIsWarning(!isWarning)}>
+          <Text style={{ color: '#fff', marginRight: 10 }}>Warning</Text>
+        </TouchableOpacity>
+      ),
     });
-  };
+  }, [navigation, isWarning, goalObj.text]);
+
+  const textStyle = [
+    styles.detailText,
+    isWarning && styles.warningText
+  ];
 
   return (
     <SafeAreaView style={sharedStyles.container}>
       <View style={sharedStyles.content}>
         <Text style={sharedStyles.title}>Goal Details</Text>
-        <Text style={styles.detailText}>Goal: {goalObj.text}</Text>
-        <Text style={styles.detailText}>ID: {goalObj.id}</Text>
+        <Text style={textStyle}>Goal: {goalObj.text}</Text>
+        <Text style={textStyle}>ID: {goalObj.id}</Text>
         {goalObj.additionalInfo && (
-          <Text style={styles.detailText}>Additional Info: {goalObj.additionalInfo}</Text>
+          <Text style={textStyle}>Additional Info: {goalObj.additionalInfo}</Text>
         )}
-        <TouchableOpacity style={sharedStyles.button} onPress={handleMoreDetails}>
-          <Text style={sharedStyles.buttonText}>More Details</Text>
+        <TouchableOpacity style={sharedStyles.button} onPress={() => navigation.goBack()}>
+          <Text style={sharedStyles.buttonText}>Go Back</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
@@ -35,5 +48,8 @@ const styles = StyleSheet.create({
   detailText: {
     fontSize: 18,
     marginBottom: 10,
+  },
+  warningText: {
+    color: 'red',
   },
 });
