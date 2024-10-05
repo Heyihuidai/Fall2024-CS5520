aded3219a4d15313a1b2d58cb55a72059ab14a47
Shuojun Chen <shuojun@Shuojuns-MacBook-Air.local>
2024-10-04 20:52:55 -0700

    add NativeStackNavigator

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Home />
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home" 
+          component={Home}
+          options={{ title: 'My Awesome App' }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
