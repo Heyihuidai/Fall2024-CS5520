dbcb8011bbdda0502072adcefb981fec515d31dc
Shuojun Chen <shuojun@Shuojuns-MacBook-Air.local>
2024-10-04 21:06:40 -0700

    update App.js with GoalDetails Screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/Home';
+import GoalDetails from './components/GoalDetails';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,11 @@ export default function App() {
           name="Home" 
           component={Home}
           options={{ title: 'My Awesome App' }}
+        />
+        <Stack.Screen 
+          name="Details" 
+          component={GoalDetails}
+          options={{ title: 'Goal Details' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
