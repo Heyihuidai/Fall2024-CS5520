4682467ce945499a5e4bcbf7994718ce63e0247d
Shuojun Chen <shuojun@Shuojuns-MacBook-Air.local>
2024-10-04 22:10:08 -0700

    add left and right buttons to the header

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/Home';
@@ -27,7 +28,7 @@ export default function App() {
         <Stack.Screen 
           name="Details" 
           component={GoalDetails}
-          options={({ route }) => ({
+          options={({ route, navigation }) => ({
             title: route.params.goalObj.text,
             headerStyle: {
               backgroundColor: '#8a2be2',
@@ -36,6 +37,16 @@ export default function App() {
             headerTitleStyle: {
               fontWeight: 'bold',
             },
+            headerLeft: () => (
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Text style={{ color: '#fff', marginLeft: 10 }}>All My Goals</Text>
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <TouchableOpacity onPress={() => console.log('Warning pressed')}>
+                <Text style={{ color: '#fff', marginRight: 10 }}>Warning</Text>
+              </TouchableOpacity>
+            ),
           })}
         />
       </Stack.Navigator>
